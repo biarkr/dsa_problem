a932a58d66a7521ab415739cdcf4f5d850ae1c4e
biku <srkr.biku@gmail.com>
2024-07-05 17:21:14 +0530

    add find pair of sum given number - sort them then use two pointer technique.

--- 1.find_a_pair_with_the_given_sum_in_an_array.cc
+++ 1.find_a_pair_with_the_given_sum_in_an_array.cc
@@ -7,10 +7,13 @@
  * 		pair found (8, 2)
  * 		
  * 		*/
-
+#include<algorithm>
 #include<iostream>
 using namespace std;
 
+/* 
+// brute force method , runing time is n^2
+//
 int find_pair( int nums[], int n, int target) {
 	
 	for(int i=0; i < n-1; i++) {
@@ -25,10 +28,29 @@ int find_pair( int nums[], int n, int target) {
 			return 0;
 }
 
+*/
+
+int find_pair(int nums[], int n, int target) {
+	sort(nums, nums+n);
+	int i=0; int j = n-1;
+	while(i < j) {
+		if(target == (nums[i]+nums[j])) {
+			cout << "Pair Found : ( " << nums[i] <<", " << nums[j] << " )" << endl;
+			return 0;
+		}
+		if (target > (nums[i]+nums[j]))
+			i++;
+		if (target < (nums[i]+nums[j]))
+			j--;
+	}
+	cout << "Pair Not Found." << endl;
+	return 0;
+}
+
 int main() {
 	int nums[] = {8, 7, 2, 5, 3, 1};
 	int target = 10;
-	int n = sizeof(nums);
+	int n = sizeof(nums)/sizeof(nums[0]);
 
 	find_pair(nums, n, target);
 	return 0;
