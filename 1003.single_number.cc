cb435db5cda06f4b237f7c9bd182ffd618be70e0
biarkr <srkr.biku@gmail.com>
2024-08-07 15:10:59 +0530

    fixed single_number sol v2.

--- 1003.single_number.cc
+++ 1003.single_number.cc
@@ -32,27 +32,24 @@ int singleNums(int nums[], int n) {
 }
 v1. ends here */ 
 
+
+
+
+/* v2. uses sorting techniquie, time complexity O(nlogn)
+ *
 int singleNums(int nums[], int n) {
 	sort(nums, nums + n);
-	for(int i = 0; i<n ; i++) {
-		cout << nums[i] << " " ;
-	}
-	cout << endl;
 	for(int i=0; i < n; i+=2) {
-		int count = 1; 
-		if(nums[i] == nums[i+1])
-			count++;
-		if(count != 2) {
+		if(nums[i] != nums[i+1]) {
 			cout << nums[i] << endl; 
 			return 0;
 		}
-		if(i == n-2) {
-			cout << nums[n-1] << endl;
-			return 0; 
-		}
 	}
-	return 1;
+	cout << nums[n-1] << endl;
+	return 0; 
 }
+v2. ends here  */
+
 
 int main() {
 	int nums[] = {4, 1, 2, 1, 2};
